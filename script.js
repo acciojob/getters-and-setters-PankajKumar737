2107795fb4434f2f189b31245fa96b1b77de486c
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-05-11 17:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 create a subclass called Student that extends the Person class
 	//complete this code
-	class Person {
-constructor(name, age){
-this.name = name;
-this.age = age;
-}
-getName() {
-return this.name;
-}
-setAge(age) {
-this.age = age;
-}
+class Person {
+	constructor(name, age){
+	this.name = name;
+	this.age = age;
+	}
+	getName() {
+	return this.name;
+	}
+	setAge(age) {
+	this.age = age;
+	}
 }
 
 class Student extends Person {
-study() {
-return (this.name + " is studying");
-}
+	study() {
+	console.log(this.name + " is studying");
+	}
 }
 
 class Teacher extends Person {
-teach() {
-return (this.name + " is teaching");
-}
+	teach() {
+	console.log(this.name + " is teaching");
+	}
 }
 
 // Do not change the code below this line
